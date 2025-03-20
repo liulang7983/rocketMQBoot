3cf292dcb1aa98aeea005329d552a83220977c3b
ming.li <1430793456@qq.com>
2025-03-20 10:05:17 +0800

    模拟重试机制

---
+++ testBoot/src/main/java/com/controller/RetryController.java
@@ -0,0 +1,28 @@
+package com.controller;
+
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author ming.li
+ * @Date 2025/3/20 10:00
+ * @Version 1.0
+ */
+@RestController
+@RequestMapping("retry")
+public class RetryController {
+
+    @Autowired
+    private RocketMQTemplate rocketMQTemplate;
+
+    @GetMapping("/send")
+    public String sendMessage() {
+        String message = "Hello, RocketMQ!";
+        // 发送消息到指定主题
+        rocketMQTemplate.convertAndSend("retryTopic", message);
+        return "Message sent successfully";
+    }
+}
